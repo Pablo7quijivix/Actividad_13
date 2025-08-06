80fb14fb5baec970d9383c221e51af883fdb8d45
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-05 18:23:53 -0600

    3 commit avances

--- Actividad_13.py
+++ Actividad_13.py
@@ -14,36 +14,56 @@ def menu_principal():
 menu_principal()
 
 try:
-    estudiantes ={}
+    estudiantes ={} #ejemoplo de usuario guardado estudiante={'123':Maria Carmen} clave el id y el valor es el nombre
     while True:
         print(menu_principal)
         opcion_principal = input(f"_ELIGA UNA OPCIÓN (1-6)_: ")
         match opcion_principal:
             case "1": #opcion uno, agregar estudiantes
+                print(f"\n___DATOS PARA EL ESTUDIANTE___")
                 while True:
-                    # parte de agregar n cantidad de estudiantes
-                    cantidad_estudiantes = int(input(f"__¿CUANTOS ESTUDIANTES DESEA INGRESAR?__: "))
-                    for i in range(cantidad_estudiantes):
-                        print(f"\n__DATOS PARA EL ESTUDIANTE_{i+1}_")
-                        id= input(f"___INGRESE EL ID DEL ESTUDIANTES (ÚNICO)___: ")
-                        name = input(f"___INGRESE NOMBRE COMPLETO___: ")
-                        carrera_programa = input(f"___INGRESE CARRERA O PROGRAMA ACADEMICO___: ")
-                    print(f"==DATOS DEL ESTUDIANTE GUARDADO EXITOSAMENTE===")
+                    #pedimos que el carnet del usuario tenga unicamente 7 caracteres, para que sea más facil de ingresarlo de nuevo.
+                    id= input(f"___INGRESE EL ID DEL ESTUDIANTES (ÚNICO, (UNICAMENTE 7 DIGITOS))___: ")
+                    id2 = id.len()
+                    if id2 == 7:
+                        print(f"__ID GUARDADO EXITOSAMENTE__")
+                        break
+                    else:
+                        print(f"_____ID NO VALIDO, DEBE CONTENER 7 CARACTERES, INTENTELO DE NUEVO_____")
 
-                    estudiantes[id]={
-                        'nombre':name,
-                        'cursos':{
-                            'nombre_curso':nombre_curso,
-                            'nota final': nota_final
+                name = input(f"___INGRESE NOMBRE COMPLETO___: ")
+                carrera_programa = input(f"___INGRESE CARRERA O PROGRAMA ACADEMICO___: ")
+                #guardamos el id y el nombre en el diccionario estudiantes.
+                estudiantes[id] = name #el id se vuelve clave y su valor es el nombre del estudiante
+                print(f"==DATOS DEL ESTUDIANTE GUARDADO EXITOSAMENTE===")
 
-                        }
+                estudiantes[id]={
+                     'cursos':{
+                        'nombre_curso':nombre_curso,
+                        'nota final': nota_final
 
                     }
 
+                }
+
             case "2": #opcion 2, agregar curso con nota
-                id2=(f"===INGRESE EL ID DEL ESTUDIANTE===: ")
+                id=(f"===INGRESE EL ID DEL ESTUDIANTE===: ")
                 nombre_curso = input(f"===INGRESE EL NOMBRE DEL CURSO===: ")
                 nota_final = input(f"===INGRESE NOTA FINAL DEL ESTUIDANTE (0-100 )===: ")
+                while True:
+                    if  0 <=nota_final<= 100:
+                        print(f"_____NOTA GUARDADA CORRECTAMENTE_____)")
+                        #el break lo colocamos en la parte del if, ya que si verifica la nota dentro del rango
+                        # ya no hay necesidad de ir al "else"
+                        break
+                    else:
+                        print(f"===NOTA GUARDADA CON EXITO===")
+                        break
+
+            case "3": #consultar estudiante
+
+
+
 
 
 
